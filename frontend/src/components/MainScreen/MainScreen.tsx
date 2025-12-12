60bb683c900737d87618af506df81c710f16df89
иван Горбунов <isdfdsf.sdfs@mail,ru>
2025-12-12 14:46:29 +0400

    сделал реализацию кнопок через обработчик событий

--- frontend/src/components/MainScreen/MainScreen.tsx
+++ frontend/src/components/MainScreen/MainScreen.tsx
@@ -1,7 +1,15 @@
 import s from "./MainScreen.module.css";
 import ladya from "../../assets/ladya.png";
+import { useState } from "react";
 
 function MainScreen() {
+  const [activeButton, setActiveButton] = useState("online");
+  const handleClickOnline = () => {
+    setActiveButton("online");
+  };
+  const handleClickOffline = () => {
+    setActiveButton("offline");
+  };
   return (
     <div className={s.main_wpapper}>
       <div className={s.info_wrapper}>
@@ -17,13 +25,33 @@ function MainScreen() {
           </p>
         </div>
         <div className={s.buttons_wrapper}>
-            <div className={s.format_buttons}>
-                <button  className={s.offline_button}>лично</button>
-                <button className={s.online_button}>онлайн</button>
-            </div>
-            <div>
-                <button className={s.sign_up}>Записаться</button>
-            </div>
+          <div className={s.format_buttons}>
+            <button
+              className={s.offline_button}
+              onClick={handleClickOffline}
+              style={{
+                background: activeButton === "offline" ? "white" : "aliceblue",
+                border: activeButton === "offline" ? "2px solid red" : "none",
+                color: activeButton === "offline" ? "red" : "black",
+              }}
+            >
+              лично
+            </button>
+            <button
+              className={s.online_button}
+              style={{
+                background: activeButton === "online" ? "white" : "aliceblue",
+                border: activeButton === "online" ? "2px solid red" : "none",
+                color: activeButton === "online" ? "red" : "black",
+              }}
+              onClick={handleClickOnline}
+            >
+              онлайн
+            </button>
+          </div>
+          <div>
+            <button className={s.sign_up}>Записаться</button>
+          </div>
         </div>
       </div>
 
